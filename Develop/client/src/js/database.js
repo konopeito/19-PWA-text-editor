efffd987738b8fbeb21774d3522d78056b969f73
Julia <juliafritsch04@gmail.com>
2022-11-14 10:22:54 -0600

    database.js todos completed

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -13,9 +13,26 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+// console.error('putDb not implemented');
+const jateDb = await openDB("jate", 1);
+const tx = jateDb.transaction("jate", "readwrite");
+const store = tx.objectStore("jate");
+const request = store.put({id: 1, value: content});
+const result = await request;
+console.log("Entry updated & saved to Database", result);
+};
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  console.log("Text has been fetched from database.");
+  const jateDb = await openDB("jate", 1);
+  const tx = jateDb.transaction("jate", "readonly");
+  const store = tx.objectStore("jate");
+  const request = store.getAll();
+  const result = await request;
+  console.log("result.value", result.value);
+  return result.value;
+};
 
 initdb();
